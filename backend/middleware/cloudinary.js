09933e2daf7f9434fd6608c3be70d8a710973789
IT21036620 <it21036620@my.sliit.lk>
2024-09-26 06:04:47 +0530

    hotfix: cloudinary unrestricted file upload destination

--- backend/middleware/cloudinary.js
+++ backend/middleware/cloudinary.js
@@ -18,3 +18,35 @@ const upload = multer({
 })
 
 export default upload
+
+//hotfix: unrescricted file upload destination
+// import multer from 'multer';
+// import path from 'path';
+
+// // Set the destination to a specific directory
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     // Specify a safe directory where files will be saved
+//     cb(null, path.join(__dirname, '../uploads/'));
+//   },
+//   filename: function (req, file, cb) {
+//     // Ensure that the filename is unique to avoid overwriting
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//     cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+//   }
+// });
+
+// const upload = multer({
+//   storage: storage,
+//   limits: {
+//     fileSize: 2 * 1024 * 1024, // 2MB max file size
+//   },
+//   fileFilter: function (req, file, cb) {
+//     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+//       return cb(new Error('Please upload an image'));
+//     }
+//     cb(null, true);
+//   }
+// });
+
+// export default upload;
